2c99420cc24168a1cd943a9cd2548387923ef05f
Jack Burke <jburke5@sheffield.ac.uk>
2025-10-11 22:58:06 +0100

    fix(url): update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jackaburke.github.io',
+  site: 'https://jackaburke.com',
 })
