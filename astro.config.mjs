636765037f4f5fd541f3f393f2e5bd725652e496
Jack Burke <jburke5@sheffield.ac.uk>
2025-10-12 01:46:24 +0100

    fix(dart-sass): silence deprecation warnings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,16 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        // Fixing issue with dart-sass deprecation warnings
+        // [https://github.com/twbs/bootstrap/issues/40962#issuecomment-2448214806]
+        scss: {
+          silenceDeprecations: ['color-functions', 'global-builtin', 'import']
+        }
+      }
+    }
+  },
   site: 'https://jackaburke.com',
 })
