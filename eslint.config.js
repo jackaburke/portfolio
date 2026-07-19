07e7535c436d418f0540865c4267b5c043695e86
Jack Burke <jburke5@sheffield.ac.uk>
2026-07-19 14:54:43 +0100

    chore(ci): add CI workflow and lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,20 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
 import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
   // add more generic rule sets here, such as:
   js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  {
+    // component frontmatter is TypeScript
+    files: ['**/*.astro'],
+    languageOptions: {
+      parserOptions: {
+        parser: tsParser
+      }
+    }
+  },
   {
     rules: {
       // override/add rules settings here, such as:
